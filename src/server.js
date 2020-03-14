225d5777660ecfa91e731d3009184f8518115425
Giacomo Moscardini <gmoscardini@webgriffe.com>
2020-03-14 19:23:01 +0100

    Revert "Unubo please"
    This reverts commit c572c6ef5681f7acc95883a6575b559116fa3475.

--- src/server.js
+++ src/server.js
@@ -1,31 +1,42 @@
-const express = require('express');
+import express from "express";
+import bodyParser from "body-parser";
+import Person from "./Model/Person.js";
+import PersonValidator from "./Service/PersonValidator.js";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 //configuration for public folder
-app.use(express.static(__dirname + '/public_html'));
+import path from "path";
+app.use(express.static(path.resolve() + "/public_html"));
 //configuration for accept json from post
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/status', (req, res) => {
+app.get("/status", (req, res) => {
   res.json({
-    status: 'ok',
+    status: "ok",
     statusCode: 200,
   })
 });
 
-app.post('/generate-fiscal-code', (req, res) => {
+app.post("/generate-fiscal-code", (req, res) => {
+  let person = new Person(req.body.firstname, req.body.lastname, req.body.birthdate, req.body.isMale, req.body.municipality);
+  let validator = new PersonValidator();
+  if (!validator.validate(person)) {
+    res.json({
+      isValid: false,
+      message: "valid-fiscal-code",
+    })
+  }
   res.json({
-    isValid: true,
-    fiscalcode: 'valid-fiscal-code',
+    isValid: false,
+    fiscalcode: "valid-fiscal-code",
   })
 });
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   res.json({
-    status: 'Page Not Found',
+    status: "Page Not Found",
     statusCode: 404,
   })
 });
