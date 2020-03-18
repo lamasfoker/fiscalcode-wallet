ee7dca156bc4cd6d9fd20b9374450e3af3088fbd
Giacomo Moscardini <gmoscardini@webgriffe.com>
2020-03-18 16:25:37 +0100

    Add information listing

--- src/public_html/views/pages/List.js
+++ src/public_html/views/pages/List.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const $ = document.querySelector.bind(document);
+import People from "../../models/People.js";
 
 export default class List{
     static render() {
@@ -15,7 +16,35 @@ export default class List{
         `
     }
 
+    static skeleton_render() {
+        return `
+            {{each(options.list)}}
+                <div class="row">
+                    <div class="col s12 m6">
+                        <div class="card blue-grey darken-1" id="card-{{@index}}">
+                            <div class="card-content white-text">
+                                <span class="card-title">{{@this.firstName}} {{@this.lastName}}</span>
+                                <p>Data di Nascita: {{@this.birthDate}}<br/>
+                                Sesso: {{if(@this.isMale)}}Uomo{{#else}}Donna{{/if}}<br/>
+                                Comune di Nascita: {{@this.municipality}}
+                                Codice FIscale: {{@this.fiscalCode}}</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="http://barcodes4.me/barcode/c39/{{@this.fiscalCode}}.png" target="_blank">Clicca qui per vedere il Codice a Barre</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            {{/each}}
+        `
+    }
+
     static after_render() {
+        //TODO: add delete behaviour
+        const people = new People();
         $('#header-title').innerText = 'Codici Fiscali Salvati';
+        $('#main-container').innerHTML = Sqrl.Render(this.skeleton_render(), {
+            list: people.getList()
+        })
     }
 }
